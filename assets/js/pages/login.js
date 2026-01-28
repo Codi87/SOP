ac5a6a1b8e1a7b33cd47faddd5f09440a66cd0b6
Codi87 <francesco.codignola@live.it>
2026-01-28 11:44:14 +0100

    Update login.js

--- assets/js/pages/login.js
+++ assets/js/pages/login.js
@@ -33,7 +33,7 @@
         nome: "Mario",
         cognome: "Rossi",
         comitato: "Pesaro",
-        qualifiche: [] // opzionale
+        qualifiche: ["OPEM"] // opzionale
       });
       localStorage.setItem("volontari", JSON.stringify(volontari));
     }
